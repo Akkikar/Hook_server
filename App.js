fbf10a17c15c8c1ae9c71e2c1ee681ecde5e8e32
AKASH_AKKI <akashkarlapudi24@email.com>
2024-04-30 15:36:56 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,88 @@
+// express-server/app.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// MongoDB Connection
+const mongoURI= process.env.MONGODB_URI;
+mongoose.connect(mongoURI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
+
+// Define counter schema and model
+const counterSchema = new mongoose.Schema({
+    count: { type: Number, default: 0 },
+    count1:{type:Number,default:0}
+},{ collection: 'counters' });
+const Counter = mongoose.model('Counters', counterSchema);
+
+
+
+// Routes
+app.get('/api/counter', async (req, res) => {
+    console.log("Reached GET method")
+    try {
+        
+        const counter = await Counter.findOne();
+        console.log(counter);
+        res.json(counter);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+app.post('/api/counter/increment/:variable', async (req, res) => {
+    try {
+        const { variable } = req.params;
+        let counter = await Counter.findOne();
+        if (!counter) {
+            counter = new Counter();
+        }
+        // counter.count++;
+        if (variable === 'count1') {
+            counter.count1++;
+          } else if (variable === 'count') {
+            counter.count++;
+          }
+        await counter.save();
+        res.json(counter);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+app.post('/api/counter/decrement/:variable', async (req, res) => {
+    try {
+        const { variable } = req.params;
+        let counter = await Counter.findOne();
+        if (!counter) {
+            counter = new Counter();
+        }
+        // counter.count++;
+        if (variable === 'count1') {
+            counter.count1--;
+          } else if (variable === 'count') {
+            counter.count--;
+          }
+        await counter.save();
+        res.json(counter);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
